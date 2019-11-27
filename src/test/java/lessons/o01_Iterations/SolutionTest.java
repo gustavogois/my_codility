d42d4ad4b1d7131594299155d99e12e50863f26d
E005169 <gustavo.menezes.gois@cgd.pt>
2019-11-27 17:01:10 +0000

    Tests, relates #1

--- src/test/java/lessons/o01_Iterations/SolutionTest.java
+++ src/test/java/lessons/o01_Iterations/SolutionTest.java
@@ -11,39 +11,43 @@ public class SolutionTest {
     Solution solution;
 
     @Before
-    void init() {
+    public void init() {
         solution = new Solution();
     }
 
     @Test
-    void solution_9_1001(String teste) {
-
+    public void solution9() {
+        // 1001
         assertThat(solution.solution(9), equalTo(2));
-        assertThat(teste.equals("s"), equalTo(Boolean.TRUE));
     }
 
     @Test
-    void solution_529_1000010001() {
+    public void solution529() {
+        // 1000010001
         assertThat(solution.solution(529), equalTo(4));
     }
 
     @Test
-    void solution_20_10100() {
+    public void solution20() {
+        // 10100
         assertThat(solution.solution(20), equalTo(1));
     }
 
     @Test
-    void solution_32_100000() {
+    public void solution32() {
+        // 100000
         assertThat(solution.solution(32), equalTo(0));
     }
 
     @Test
-    void solution_1041_10000010001() {
+    public void solution1041() {
+        // 10000010001
         assertThat(solution.solution(1041), equalTo(5));
     }
 
-    //@Test(expected = Test.None.class)
-    void solution_max() {
+    @Test(expected = Test.None.class)
+    public void solution_max() {
+
         solution.solution(2147483647);
     }
 
