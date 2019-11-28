542a39861fa998d230701f5b436e05958e650897
E005169 <gustavo.menezes.gois@cgd.pt>
2019-11-28 13:07:47 +0000

    2nd delivery with 100% score, relates #1

--- src/main/java/lessons/o01_Iterations/Solution.java
+++ src/main/java/lessons/o01_Iterations/Solution.java
@@ -5,20 +5,41 @@ public class Solution {
 
         String binary = Integer.toBinaryString(n);
 
-        boolean isCounting = false;
-        int count = 0, result = 0;
-        for (int i = 0 ; i < binary.length() ; i++) {
-            if (binary.charAt(i) == '1') {
-                if (isCounting) {
-                    result = count;
-                    count = 0;
-                }
-                isCounting = !isCounting;
-            } else if (isCounting) {
-                count++;
-            }
-        }
+        int result = binaryGap(binary);
 
         return result;
+    }
+
+    public int binaryGap(String binary) {
+        if (binary.length() < 3) {
+            return 0;
+        }
+        boolean isCounting = false;
+        int count = 0, result = 0;
+        for (int i = 1 ; i < binary.length() - 1; i++) {
+            if (startCounting(binary, i)) {
+                isCounting = true;
+                count++;
+            }
+            if (endCounting(binary, i)) {
+                result = count > result ? count : result;
+                count = 0;
+                isCounting = false;
+            }
+            count = shouldIncrement(binary, isCounting, i) ? ++count : count;
+        }
+        return result;
+    }
+
+    private boolean shouldIncrement(String binary, boolean isCounting, int i) {
+        return isCounting && binary.charAt(i) == '0';
+    }
+
+    private boolean endCounting(String binary, int i) {
+        return binary.charAt(i) == '0' && binary.charAt(i + 1) == '1';
+    }
+
+    private boolean startCounting(String binary, int i) {
+        return binary.charAt(i) == '0' && binary.charAt(i - 1) == '1';
     }
 }
--- src/test/java/lessons/o01_Iterations/SolutionTest.java
+++ src/test/java/lessons/o01_Iterations/SolutionTest.java
@@ -51,4 +51,46 @@ public class SolutionTest {
         solution.solution(2147483647);
     }
 
+    @Test
+    public void solution328() {
+        // 101001000
+        assertThat(solution.solution(328), equalTo(2));
+    }
+
+    @Test
+    public void solution1162() {
+        // 10010001010
+        assertThat(solution.solution(1162), equalTo(3));
+    }
+
+    @Test
+    public void solution51712() {
+        // 110010100000000
+        assertThat(solution.solution(51712), equalTo(2));
+    }
+
+    @Test
+    public void binaryGap11111() {
+        assertThat(solution.binaryGap("11111"), equalTo(0));
+    }
+
+    @Test
+    public void binaryGap0000() {
+        assertThat(solution.binaryGap("0000"), equalTo(0));
+    }
+
+    @Test
+    public void binaryGap101() {
+        assertThat(solution.binaryGap("101"), equalTo(1));
+    }
+
+    @Test
+    public void binaryGap1010010() {
+        assertThat(solution.binaryGap("1010010"), equalTo(2));
+    }
+
+    @Test
+    public void binaryGap01000010010() {
+        assertThat(solution.binaryGap("01000010010"), equalTo(4));
+    }
 }
