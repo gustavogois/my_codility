178c4fac44e3002e12e91290d6555f566075d5df
E005169 <gustavo.menezes.gois@cgd.pt>
2019-11-28 10:35:12 +0000

    1a entrega com 40% de score, relates #1

--- src/main/java/lessons/o01_Iterations/Solution.java
+++ src/main/java/lessons/o01_Iterations/Solution.java
@@ -3,6 +3,22 @@ package lessons.o01_Iterations;
 public class Solution {
     public int solution(int n) {
 
-        return n;
+        String binary = Integer.toBinaryString(n);
+
+        boolean isCounting = false;
+        int count = 0, result = 0;
+        for (int i = 0 ; i < binary.length() ; i++) {
+            if (binary.charAt(i) == '1') {
+                if (isCounting) {
+                    result = count;
+                    count = 0;
+                }
+                isCounting = !isCounting;
+            } else if (isCounting) {
+                count++;
+            }
+        }
+
+        return result;
     }
 }
